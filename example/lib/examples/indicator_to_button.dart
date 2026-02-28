2cd3b26f64399238e73cfad80cdaa8a756128a62
milad <milad.akarie@gmail.com>
2026-02-28 16:18:19 +0300

    feat: Add IndicatorToButton widget for enhanced UI interaction

---
+++ example/lib/examples/indicator_to_button.dart
@@ -0,0 +1,116 @@
+import 'package:cue/cue.dart';
+import 'package:flutter/material.dart';
+
+class IndicatorToButton extends StatefulWidget {
+  const IndicatorToButton({super.key});
+
+  @override
+  State<IndicatorToButton> createState() => _IndicatorToButtonState();
+}
+
+class _IndicatorToButtonState extends State<IndicatorToButton> {
+  final _cuePageController = CuePageController(viewportFraction: .8);
+
+  @override
+  dispose() {
+    _cuePageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 200,
+          child: PageView.builder(
+            controller: _cuePageController,
+            itemCount: 5,
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Cue.indexed(
+                index: index,
+                controller: _cuePageController,
+                child: Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                  elevation: .2,
+                  shape: RoundedSuperellipseBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        SizedBox(
+          height: 88,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (var i = 0; i < 5; i++)
+                Builder(
+                  builder: (context) {
+                    final isLast = i == 4;
+                    return Cue.indexed(
+                      index: i,
+                      controller: _cuePageController,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(32),
+                          ),
+                          child: Actor(
+                            effects: [
+                              SizeEffect(
+                                from: .square(10),
+                                to: isLast ? .fromHeight(44) : NSize(38, 10),
+                                allowOverflow: isLast,
+                              ),
+                              if (isLast) ...[
+                                ScaleEffect(from: 0),
+                                SlideEffect(from: Offset(-1, 0)),
+                              ],
+                            ],
+                            child: isLast
+                                ? Padding(
+                                    padding: const .symmetric(horizontal: 16),
+                                    child: Row(
+                                      mainAxisAlignment: .center,
+                                      children: [
+                                        Text(
+                                          'Let’s Go',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(color: Colors.white, fontSize: 15),
+                                        ),
+                                        SizedBox(width: 6),
+                                        Icon(
+                                          Icons.arrow_forward_ios_rounded,
+                                          color: Colors.white,
+                                          size: 14,
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                : SizedBox(),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:cue/cue.dart';
 import 'package:example/examples/delete_confirmation.dart';
+import 'package:example/examples/indicator_to_button.dart';
 import 'package:example/examples/three_dots_action.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -50,102 +51,74 @@ class __OnChangeDemoState extends State<_OnChangeDemo> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-      // backgroundColor: theme.colorScheme.surfaceContainer,
+      backgroundColor: theme.colorScheme.surfaceContainer,
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.only(top: 48, bottom: 60),
-        child: SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: .end,
-            children: [
-              SizedBox(
-                height: 200,
-                child: PageView.builder(
-                  controller: _cuePageController,
-                  itemCount: 5,
-                  physics: BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return Cue.indexed(
-                      index: index,
-                      controller: _cuePageController,
-                      child: Card(
-                        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                        elevation: .2,
-                        shape: RoundedSuperellipseBorder(
-                          borderRadius: BorderRadius.circular(24),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [],
+        child: Center(
+          child: Cue.onToggle(
+            toggled: _checked,
+            motion: .simulation(Spring.bouncy(damping: 16)),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(32),
+              onTap: () {
+                setState(() {
+                  _checked = !_checked;
+                });
+              },
+              child: SizedBox.square(
+                dimension: 44,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(32),
+                  child: Stack(
+                    clipBehavior: Clip.antiAlias,
+                    children: [
+                      SlideActor.y(
+                        from: 0,
+                        to: 1,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(32),
+                            color: Colors.white,
                           ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 88,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    for (var i = 0; i < 5; i++)
-                      Builder(
-                        builder: (context) {
-                          final isLast = i == 4;
-                          return Cue.indexed(
-                            index: i,
-                            controller: _cuePageController,
-                            child: Padding(
-                              padding: const EdgeInsets.all(4.0),
+                          child: Center(
+                            child: SizedBox.square(
+                              dimension: 20,
                               child: DecoratedBox(
                                 decoration: BoxDecoration(
-                                  color: Colors.black,
                                   borderRadius: BorderRadius.circular(32),
-                                ),
-                                child: SizeActor(
-                                  from: .square(10),
-                                  to: isLast ? .fromHeight(44) : NSize(38, 10),
-                                  allowOverflow: isLast,
-                                  child: isLast
-                                      ? ScaleActor(
-                                          from: 0,
-                                          child: SlideActor(
-                                            from: Offset(-1, 0),
-                                            child: Padding(
-                                              padding: const .symmetric(horizontal: 16),
-                                              child: Row(
-                                                mainAxisAlignment: .center,
-                                                children: [
-                                                  Text(
-                                                    'Let’s Go',
-                                                    textAlign: TextAlign.center,
-                                                    style: TextStyle(color: Colors.white, fontSize: 15),
-                                                  ),
-                                                  SizedBox(width: 6),
-                                                  Icon(
-                                                    Icons.arrow_forward_ios_rounded,
-                                                    color: Colors.white,
-                                                    size: 14,
-                                                  ),
-                                                ],
-                                              ),
-                                            ),
-                                          ),
-                                        )
-                                      : SizedBox(),
+                                  color: Colors.red,
                                 ),
                               ),
                             ),
-                          );
-                        },
+                          ),
+                        ),
                       ),
-                  ],
+                      SlideActor.y(
+                        from: -1,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(32),
+                            color: Colors.red,
+                          ),
+                          child: Center(
+                            child: SizedBox.square(
+                              dimension: 20,
+                              child: DecoratedBox(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(6),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
